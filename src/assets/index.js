8b502e190cb654fdd7c37b1f2e157dca63f0b398
Deepak-200259 <deepwarriors2002@gmail.com>
2025-07-27 15:29:20 +0530

    Properties Integrated for SHANK

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,6 +8,22 @@ import inactiveHead from "./inactive-head.svg";
 import inactiveShank from "./inactive-shank.svg";
 import inactiveStone from "./inactive-stone.svg";
 import inactiveOther from "./inactive-other.svg";
+import plain from "./plain.png";
+import cathedral from "./cathedral.png";
+import knifeEdge from "./knife-edge.png";
+import split from "./split.png";
+import twisted from "./twisted.png";
+import widePlain from "./wide-plain.png";
+import channel from "./channel.png";
+import plateProng from "./plateprong.png";
+import whiteGold from "./white.png";
+import yellowGold from "./yellow.png";
+import roseGold from "./rose.png";
+import platinum from "./platinum.png";
+import regular from "./regular.png";
+import script from "./script.png";
+import italics from "./italics.png";
+import roman from "./roman.png";
 
 export {
   bezelHead,
@@ -20,6 +36,22 @@ export {
   inactiveShank,
   inactiveStone,
   inactiveOther,
+  plain,
+  cathedral,
+  knifeEdge,
+  split,
+  twisted,
+  widePlain,
+  channel,
+  plateProng,
+  whiteGold,
+  yellowGold,
+  roseGold,
+  platinum,
+  regular,
+  script,
+  italics,
+  roman
 };
 // export default function Assets(){
 //     return {
--- src/components/PropertiesSelector.jsx
+++ src/components/PropertiesSelector.jsx
@@ -1,7 +1,104 @@
 export default function PropertiesSelector(props) {
-    return (
-        <>
-            <h1>{props.heading}</h1>
-        </>
-    );
+  return (
+    <div className="properties-selector-tab">
+      {props.content.includesEngraving ? (
+        <EngravingPropertySelector property={props.content} />
+      ) : (
+        <div className="properties-section">
+          <h4 className="property-main-heading">
+            {props.content.name}{" "}
+            <span className="selected-property-option">
+              {props.currentStyle ? props.currentStyle : props.currentMetal}
+            </span>
+          </h4>
+          <div className="property-options">
+            {!props.content.mainOptionsHasText
+              ? props.content.options.map((option) => {
+                  return (
+                    <div className="property-option" key={option.name}>
+                      <img
+                        className="property-icon"
+                        src={option.img}
+                        alt={option.name}
+                      />
+                    </div>
+                  );
+                })
+              : null}
+          </div>
+          <h5 className="property-sub-heading">
+            {props.content.subHeadingName}{" "}
+            <span className="selected-property-option">
+              {props.currentSideSetting
+                ? props.currentSideSetting
+                : props.currentCarat}
+            </span>
+          </h5>
+          <div className="property-options">
+            {!props.content.subOptionsHasText
+              ? props.content.subOptions.map((option) => {
+                  return (
+                    <div className="property-option" key={option.name}>
+                      <img
+                        className="property-icon"
+                        src={option.img}
+                        alt={option.name}
+                      />
+                    </div>
+                  );
+                })
+              : props.content.subOptions.map((option) => {
+                  return (
+                    <div className="property-option" key={option.name}>
+                      <p className="property-icon">{option.name}</p>
+                    </div>
+                  );
+                })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function EngravingPropertySelector({ property }) {
+  return (
+    <>
+      <div className="properties-section">
+        <h4 className="property-main-heading">{property.name}</h4>
+        <input
+          type="text"
+          name="EngravingText"
+          id="engraving-text-input"
+          placeholder="Type here"
+          className="engraving-text-input"
+        />
+        <h5 className="property-sub-heading">{property.subHeading1}</h5>
+        <div className="sub-options-container">
+          {property.subOptions1.map((option, index) => {
+            return (
+              <div className={`${"font-option"} ${index === 0 ? "selected" : ""}`}>
+                <img
+                  className="font-icon"
+                  src={option.img}
+                  alt={option.name}
+                  key={option.name}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <h5 className="property-sub-heading">{property.subHeading2}</h5>
+        <div className="symbols">
+          {property.subOptions2.map((option) => {
+            return (
+              <p className="symbol" key={option}>
+                {option}
+              </p>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
