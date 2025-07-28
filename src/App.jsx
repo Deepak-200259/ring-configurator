5963beab6a5ff9be87d842e2a2563fe200e4dbb0
Aakash Shukla <aakashshukla.vt@gmail.com>
2025-07-28 11:06:24 +0530

    Testing commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavigationBar from "./components/NavigationBar";
 import Experience from "./experience/Experience";
 
 export default function App() {
+  console.log("App Testing");
   return (
     <>
       <NavigationBar />
