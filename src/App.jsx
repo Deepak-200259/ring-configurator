5ee36119d4749749aff5e3558da066c08616ec68
Aakash Shukla <aakashshukla.vt@gmail.com>
2025-07-28 11:12:44 +0530

    Testing commit remove

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import NavigationBar from "./components/NavigationBar";
 import Experience from "./experience/Experience";
 
 export default function App() {
-  console.log("App Testing");
   return (
     <>
       <NavigationBar />
